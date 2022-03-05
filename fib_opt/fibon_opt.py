63d8ed113e2ee65ad83922638d82f763d5bb28f7
Darren Chua <44729635+darrenCWJ@users.noreply.github.com>
2022-03-05 19:14:17 +0800

    Update fibon_opt.py

--- fib_opt/fibon_opt.py
+++ fib_opt/fibon_opt.py
@@ -2,7 +2,7 @@ def fib(n, memo = {}):
     print(n)
     if n in memo: return memo[n]
     if n<=2 : return 1
-    memo[n] = fib(n-1) + fib(n-2)
+    memo[n] = fib(n-1, memo) + fib(n-2, memo)
     return memo[n]
 ##  1, 1, 2, 3, 5, 8, 13, 21, 34
 
